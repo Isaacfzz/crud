4eaa32822cced40d586676d1458157c517f82bc0
Isaac <isaac11santos@gmail.com>
2023-10-20 21:51:01 -0300

    substitui armazenamento em memória por banco h2

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -1,9 +1,8 @@
 package br.com.belval.crud.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ListIterator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,140 +12,131 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.belval.crud.model.Produto;
+import br.com.belval.crud.repository.ProdutoRepository;
 
 @Controller
 public class ProdutoController {
-	
-	private static List<Produto> lista = new ArrayList<>();
-	private static int proxId = 1;
+
+//	private static List<Produto> lista = new ArrayList<>();
+//	private static int proxId = 1;
+
+	@Autowired
+	private ProdutoRepository repository;
 
 	@GetMapping("/produto/novo")
 	public String novo(Model model) {
 		model.addAttribute("produto", new Produto());
 		return "produto";
 	}
-	
+
 	@GetMapping("/produto/{id}/edit")
 	public String editar(@PathVariable int id, Model model) {
-		
-		Produto produto = buscarPorId(id);
-		
+
+		Produto produto = repository.findById(id);
+
 		if (produto == null) {
 			return "produto-nao-encontrado";
 		}
-		
+
 		model.addAttribute("produto", produto);
-		
-		
+
 		return "produto";
 	}
-	
+
 	@PostMapping("/produto/novo")
 	public ModelAndView novo(Produto produto, RedirectAttributes redirectAttributes) {
 		ModelAndView modelAndView = new ModelAndView("redirect:/produto/list");
-		
+
 		if (produto.getId() == 0) {
-			insert(produto);
+			// insert(produto);
 			redirectAttributes.addFlashAttribute("msg", "Novo produto criado!");
 		} else {
-			update(produto);
+			// update(produto);
 			redirectAttributes.addFlashAttribute("msg", "produto atualizado");
 		}
-		
+		repository.save(produto);
 		return modelAndView;
 	}
 
-	private void insert(Produto produto) {
-		produto.setId(proxId++);
-		lista.add(produto);
-	}
-
-	private void update(Produto produto) {
-		ListIterator<Produto> it = lista.listIterator();
-		while(it.hasNext()) {
-			Produto encontrado = it.next();
-			if (encontrado.getId() == produto.getId()) {
-				it.remove();
-				it.add(produto);
-			}
-		}
-	}
+//	private void insert(Produto produto) {
+//		produto.setId(proxId++);
+////		lista.add(produto);
+//		repository.save(produto);
+//	}
+//
+//	private void update(Produto produto) {
+//		repository.save(produto);
+//		ListIterator<Produto> it = lista.listIterator();
+//		while(it.hasNext()) {
+//			Produto encontrado = it.next();
+//			if (encontrado.getId() == produto.getId()) {
+//				it.remove();
+//				it.add(produto);
+//			}
+//		}
+//	}
 
 	@GetMapping("/produto/list")
 	public String list(Model model) {
-		model.addAttribute("produtos", lista);
+//		model.addAttribute("produtos", lista);
+		model.addAttribute("produtos", repository.findAll());
 		return "lista-produtos";
 	}
-	
+
 	@GetMapping("/produto/{id}")
 	public String detalhe(@PathVariable int id, Model model) {
-		Produto produto = buscarPorId(id);
+		Produto produto = repository.findById(id);			
 		
 		if (produto != null) {
 			model.addAttribute("novoProduto", produto);
 			return "novo-produto-criado";
 		}
-		
+
 		return "produto-nao-encontrado";
 	}
 
-	private Produto buscarPorId(int id) {
-		Produto encontrou = null;
-		for(Produto p : lista) {
-			if (p.getId() == id) {
-				//encontrou o produto solicitado
-				encontrou = p;
-				break;
-			}
-		}
-		return encontrou;
-	}
-	
+//	private Produto ♦(int id) {
+//		Produto encontrou = null;
+//		for (Produto p : lista) {
+//			if (p.getId() == id) {
+//				// encontrou o produto solicitado
+//				encontrou = p;
+//				break;
+//			}
+//		}
+//		return encontrou;
+//	}
 
-	private String excluirProduto(int id) {
-		ListIterator<Produto> it = lista.listIterator();
-		while(it.hasNext()) {
-			Produto encontrado = it.next();;
-			if (encontrado.getId() ==id) {
-				it.remove();;
+//	private String excluirProduto(int id) {
+//		ListIterator<Produto> it = lista.listIterator();
+//		while (it.hasNext()) {
+//			Produto encontrado = it.next();
+//			;
+//			if (encontrado.getId() == id) {
+//				it.remove();
+//				;
+//
+//			}
+//		}
+//		return "lista-produtos";
+//
+//	}
 
-			}
-		}
-		return "lista-produtos";
-	
-	}
-	
-	
-	/*@GetMapping("/produto/{id}/delete")
-	public String delete(@PathVariable int id, Model model) {
-		
-		Produto produto = buscarPorId(id);
-		
-		if (produto == null) {
-			return "produto-nao-encontrado";
-		}
-		
-		model.addAttribute("produto", produto);
-		
-		
-		return "lista-produtos";
-	}*/
-	
+
 	@GetMapping("/produto/{id}/delete")
-    public String delete(@PathVariable int id, RedirectAttributes redirectAttributes) {
-        String produto = excluirProduto(id);
+	public ModelAndView delete(@PathVariable int id, RedirectAttributes redirectAttributes) {
+		ModelAndView view = new ModelAndView("redirect:/produto/list");
+		//		String produto = excluirProduto(id);
+//
+//		if (produto == null) {
+//			return "produto-nao-encontrado"; // Retorna a página de produto não encontrado
+//		}
 
-        if (produto == null) {
-            return "produto-nao-encontrado"; // Retorna a página de produto não encontrado
-        }
+		// Realiza a exclusão do produto
+		repository.deleteById(id);
+		redirectAttributes.addFlashAttribute("msg", "Produto excluído");
 
-        // Realiza a exclusão do produto
-        excluirProduto(id);
-        redirectAttributes.addFlashAttribute("msg","Produto excluído");
-       
-        
-        return "redirect:/produto/list"; // Redireciona para a página de lista de produtos após a exclusão
-    }
+		return view; // Redireciona para a página de lista de produtos após a exclusão
+	}
 
-	
 }
